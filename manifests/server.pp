f8e11ce7a1df3ab5ea2ea1c62c037a5b2bf6db13
philicious <the.cypher@gmail.com>
2013-06-25 01:37:50 +0200

    fixed data dir not being initialized correctly

--- manifests/server.pp
+++ manifests/server.pp
@@ -35,12 +35,23 @@ class zookeeper::server(
 
     # Get this host's $myid from the $fqdn in the $zookeeper_hosts hash.
     $myid = $::zookeeper::hosts[$::fqdn]
+
+    # init the zookeeper data dir
+    exec { 'zookeeper-server-initialize':
+        command => "/usr/bin/zookeeper-server-initialize --myid=${myid}",
+        unless  => "/usr/bin/test -f $::zookeeper::data_dir/myid",
+        user    => "zookeeper",
+        require => Package['zookeeper'],
+    }
+
+    # replace the generated myid file with a link to /etc/zookeepeer/conf/
     file { '/etc/zookeeper/conf/myid':
         content => $myid,
     }
     file { "${::zookeeper::data_dir}/myid":
         ensure  => 'link',
         target  => '/etc/zookeeper/conf/myid',
+        require => Exec['zookeeper-server-initialize'],
     }
 
     service { 'zookeeper-server':
@@ -48,8 +59,7 @@ class zookeeper::server(
         require    => [
             Package['zookeeper-server'],
             Package['zookeeper'],
-            File[ $::zookeeper::data_dir],
-            File["${::zookeeper::data_dir}/myid"],
+            Exec['zookeeper-server-initialize'],
         ],
         hasrestart => true,
         hasstatus  => true,
